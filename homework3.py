82dab313734ce56a400d2b4a446ec25eeaf870e5
Ann109 <andersonhloa@gmail.com>
2024-06-20 19:33:38 +0300

    Update homework3.py

--- homework3.py
+++ homework3.py
@@ -1,13 +1,12 @@
-from for3 import h1 # я захотела применить знания по недавней теме импортирования кода и создала ещё один документ с
+from for3 import h1, h2 # я захотела применить знания по недавней теме импортирования кода и создала ещё один документ с
 # типом здания и этажом который потом импортировала сюда
 
 class Building:
-        def __init__(self, numberOfFloors: int, buildingType: str):
-                self.numberOfFloors = numberOfFloors
-                self.buildingType = buildingType
-        def __eg__(self, other): # self-первый участник, other - второй участник
-                return self.numberOfFloors == self.buildingType
+    def __init__(self, numberOfFloors: int, buildingType: str):
+        self.numberOfFloors = numberOfFloors
+        self.buildingType = buildingType
+    def __eg__(self, other): # self-первый участник, other - второй участник
+         return self.numberOfFloors == other.numberOfFloors and self.buildingType == other.buildingType
 
 
-print(h1.numberOfFloors == h1.buildingType)
-
+print(print(h1 == h2)
