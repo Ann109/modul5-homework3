191a6650926677fe2300ada8f460ef09ab7f8de3
Ann109 <andersonhloa@gmail.com>
2024-06-20 19:34:21 +0300

    Update for3.py

--- for3.py
+++ for3.py
@@ -1,7 +1,10 @@
 class Building:
-        def __init__(self, numberOfFloors: int, buildingType: str):
-                self.numberOfFloors = numberOfFloors
-                self.buildingType = buildingType
+    def __init__(self, numberOfFloors: int, buildingType: str):
+        self.numberOfFloors = numberOfFloors
+        self.buildingType = buildingType
+    def __eq__(self, other):      # self-первый участник, other - второй участник
+        return self.numberOfFloors == other.numberOfFloors and self.buildingType == other.buildingType
 
 
 h1 = Building('apartment house', 18)
+h2 = Building('apartment house', 18)
